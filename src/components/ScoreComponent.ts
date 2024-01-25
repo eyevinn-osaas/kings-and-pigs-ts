86a354ac6d63a6c2dfd27acbb2b32c25c50e369d
Benjamin Wallberg <me@bwallberg.com>
2024-01-25 22:24:51 +0100

    fix: highscore not updating

--- src/components/ScoreComponent.ts
+++ src/components/ScoreComponent.ts
@@ -2,22 +2,27 @@ import { Component } from "../ecs";
 
 const HIGHSCORE_KEY = "highscore";
 
-let highscore: string = "0";
-try {
-	highscore = localStorage.getItem(HIGHSCORE_KEY) ?? "0";
-} catch (e) {}
+function getHighscore() {
+  let highscore: string = "0";
+  try {
+    highscore = localStorage.getItem(HIGHSCORE_KEY) ?? "0";
+  } catch (e) {}
+
+  return +highscore;
+}
 
 export function updateHighscore(score: number) {
-	if (score > +highscore) {
-		try {
-			localStorage.setItem(HIGHSCORE_KEY, highscore);
-		} catch (e) {}
-	}
+  const highscore = getHighscore();
+  if (score > highscore) {
+    try {
+      localStorage.setItem(HIGHSCORE_KEY, score.toString());
+    } catch (e) {}
+  }
 }
 
 export default class ScoreComponent extends Component {
-	score: number = 0;
-	highscore: number = Number(highscore);
+  score: number = 0;
+  highscore: number = +getHighscore();
 
-	public destroy(): void {}
+  public destroy(): void {}
 }
